135a5f8b9bc1c8c591fbc23c9c50d1b50180c97f
naison <895703375@qq.com>
2021-08-04 10:54:33 +0800

    update version

---
+++ a.sh
@@ -0,0 +1,11 @@
+git checkout dev
+        cd docs/changelogs
+        git config user.email "895703375@qq.com"
+        git config user.name "naison"
+        time=${{ env.RELEASE_VERSION }}" ("$(date "+%Y-%m-%d")")"
+        echo $time
+        sed -i "s/{{Next}}/$time/g" *.md
+        head 0.4.x.md
+        cd ../../
+        git add .
+        git commit -m "update version"
