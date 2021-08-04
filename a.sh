7906831f89075672abab7abfcfbf1298f5d67b9e
naison <895703375@qq.com>
2021-08-04 10:55:30 +0800

    update version

--- a.sh
+++ a.sh
@@ -2,7 +2,7 @@ git checkout dev
         cd docs/changelogs
         git config user.email "895703375@qq.com"
         git config user.name "naison"
-        time=${{ env.RELEASE_VERSION }}" ("$(date "+%Y-%m-%d")")"
+        time="sdf ("$(date "+%Y-%m-%d")")"
         echo $time
         sed -i "s/{{Next}}/$time/g" *.md
         head 0.4.x.md
